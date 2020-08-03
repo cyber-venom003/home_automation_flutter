61e5a004037b11be505f8edeb1dad5e879b71c46
cyber-venom003 <tech.worm.1728@gmail.com>
2020-08-04 01:29:22 +0530

    Auth Status listening functionality added.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -12,73 +12,86 @@ class _SwitchDashBoardState extends State<SwitchDashBoard> {
   bool userAuth;
   @override
   Widget build(BuildContext context) {
-//    userRef.once().then((DataSnapshot snapShot) {
-//      userAuth = snapShot.value['isAuthenticated'];
-//    });
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color(0xff008f30),
-            title: Text('Dashboard'),
-            leading: IconButton(
-                icon: Icon(Icons.exit_to_app),
-                onPressed: () {
-                  AuthService.signOutUser();
-                  Navigator.pop(context);
-                }
-            ),
-          ),
-          body: Padding(
-            padding: EdgeInsets.symmetric(vertical: 10 , horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                    'Your Auth Status',
-                    style: TextStyle(
-                      fontSize: 20,
-                    )
-                ),
-//                FlatButton(
-//                  onPressed: null,
-//                  child: Container(
-//                    width: 300,
-//                    height: 55,
-//                    decoration: BoxDecoration(
-//                      borderRadius: BorderRadius.circular(26.0),
-//                      color: const Color(0xff00690f),
-//                      border:
-//                      Border.all(width: 1.0, color: const Color(0xfffcf3ee)),
-//                      boxShadow: [
-//                        BoxShadow(
-//                          color: const Color(0xe6c4ffcd),
-//                          offset: Offset(0, 3),
-//                          blurRadius: 6,
-//                        ),
-//                      ],
-//                    ),
-//                    child: Row(
-//                      mainAxisAlignment: MainAxisAlignment.center,
-//                      children: <Widget>[
-//                        new Text(
-//                          'Revoke Auth Status',
-//                          textAlign: TextAlign.center,
-//                          style: TextStyle(
-//                            fontFamily: "Segoe UI",
-//                            fontSize: 25,
-//                            color:Color(0xffffffff),
-//                          ),
-//                        ),
-//                        SizedBox(width: 7.5),
-//                        Icon(Icons.remove_circle_outline , size: 25 , color: Color(0xffffffff)),
-//                      ],
-//                    ),
-//                  ),
-//                ),
+            appBar: AppBar(
+              backgroundColor: Color(0xff008f30),
+              title: Text('Dashboard'),
+              leading: IconButton(
+                  icon: Icon(Icons.exit_to_app),
+                  onPressed: () {
+                    AuthService.signOutUser();
+                    Navigator.pop(context);
+                  }
+              ),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.record_voice_over),
+                    onPressed: () {
+                      userRef.child('isAuthenticated').onValue.listen((event) {
+                        var snapshot = event.snapshot;
+                        setState(() {
+                          userAuth = snapshot.value;
+                        });
+                      });
+                      print(userAuth);
+                    }
+                )
               ],
             ),
-          )
+            body: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10 , horizontal: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                      'Your Auth Status: ${userAuth == null ? 'Click on listen to see' : userAuth }',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )
+                  ),
+                FlatButton(
+                  onPressed: () {
+                    userRef.child('isAuthenticated').set(false);
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(26.0),
+                      color: const Color(0xff00690f),
+                      border:
+                      Border.all(width: 1.0, color: const Color(0xfffcf3ee)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xe6c4ffcd),
+                          offset: Offset(0, 3),
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Text(
+                          'Revoke RFID Auth ',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: "Segoe UI",
+                            fontSize: 25,
+                            color:Color(0xffffffff),
+                          ),
+                        ),
+                        SizedBox(width: 7.5),
+                        Icon(Icons.remove_circle_outline , size: 25 , color: Color(0xffffffff)),
+                      ],
+                    ),
+                  ),
+                ),
+                ],
+              ),
+            )
         )
     );
   }
